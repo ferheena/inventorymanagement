71a281eb8faf1d61ef8f80fa4d816eba6777eb90
ferheena <feroz8386@gmail.com>
2024-10-15 12:30:30 +0530

    5

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "aqlite:///./test.db"
+    DATABASE_URL: str = "sqlite:///./test.db"
 
 settings = Settings()
