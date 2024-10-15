3e83d5029a2ebc7ffd06dd96620516c9c28d1b50
ferheena <feroz8386@gmail.com>
2024-10-15 13:01:44 +0530

    6

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
+import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
-
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- run.py
+++ run.py
@@ -2,9 +2,14 @@ from importlib import reload
 from turtle import back
 import uvicorn
 from app.db.database import Base, engine
+from app.main import app
 
+def create_tables():
+    Base.metadata.create_all(bind=engine) # type: ignore
 
 if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
+
+    create_tables()
+    
 
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
